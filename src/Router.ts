46d10d816d6865c73fcae9991cf274e775d627b9
Suhail Abood <suhailabood@Suhails-MacBook-Pro.local>
2017-02-01 04:44:26 +0000

    Fixes typo in typings.

--- src/Router.ts
+++ src/Router.ts
@@ -36,6 +36,7 @@ export interface RouterProps{
      * from one route to another. 
      */
     dataStore?:DataStore;
+    children?:any;
     /**
      * A callback to be called everytime the route changes. 
      * Useful for integration with Redux and StrikeJS applications. 
