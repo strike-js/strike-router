d55c97eea478e8baf16dcd33f2b7c41434334389
Suhail Abood <suhailabood@Suhails-MacBook-Pro.local>
2017-02-01 10:04:36 +0000

    Exports AuthRoute.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import {Router} from './Router'; 
-import {Route,IndexRoute,NotFoundRoute,Redirect} from './Route';
+import {Route,IndexRoute,NotFoundRoute,Redirect,AuthRoute} from './Route';
 import {hashHistory,popStateHistory} from './History'; 
-import {identity,createDataStore} from './Util'; 
+import {identity,createDataStore,getSet,parseRoute} from './Util'; 
 
 export {Router,Route,IndexRoute,NotFoundRoute,
     Redirect,hashHistory,popStateHistory,
-    identity,createDataStore}
+    identity,createDataStore,AuthRoute,getSet,
+    parseRoute}
 
 (function(StrikeJS:any){
     if (window){
@@ -15,6 +16,7 @@ export {Router,Route,IndexRoute,NotFoundRoute,
         StrikeJS.Redirect = Redirect;
         StrikeJS.hashHistory = hashHistory;
         StrikeJS.identity = identity; 
+        StrikeJS.AuthRoute = AuthRoute; 
         StrikeJS.popStateHistory = popStateHistory; 
         StrikeJS.createDataStore = createDataStore; 
     }
